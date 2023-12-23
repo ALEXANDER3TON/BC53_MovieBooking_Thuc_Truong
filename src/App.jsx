0273818cc5acc295d6dd3a7a558125840fc70c00
QuangTruong <nqtruong2210@gmail.com>
2023-12-24 03:05:01 +0700

    update useContext

--- src/App.jsx
+++ src/App.jsx
@@ -9,29 +9,31 @@ import BookingPagse from "./Module/BookingPages/BookingPagse";
 import Register from "./Module/Auth/Register";
 import LogIn from "./Module/Auth/LogIn";
 import AdminLayout from "./Layout/AdminLayout/AdminLayout";
-import AddMovie from "./Module/Admin/AdminMovie/AddMovie";
+
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import AdminHeader from "./Components/Admin/AdminHeader/AdminHeader";
-import AdminMovieTable from "./Components/Admin/AdminMenu/AdminMovieTable";
+
+import { UserProvider } from "./contexts/useContext/useContext";
 
 function App() {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <BrowserRouter>
-        <Routes>
-          <Route path={PATH.HOME} element={<MainLayout />}>
-            <Route index element={<HomeModule />} />
-            <Route path={PATH.MOVIE_DETAILS} element={<Details />} />
-            <Route path={PATH.BOOKING_MOVIE} element={<BookingPagse />} />
-            <Route path={PATH.REGISTER} element={<Register />} />
-            <Route path={PATH.LOG_IN} element={<LogIn />} />
-          </Route>
-          <Route path={PATH.ADMIN} element={<AdminLayout />}></Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </LocalizationProvider>
+    <UserProvider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <BrowserRouter>
+          <Routes>
+            <Route path={PATH.HOME} element={<MainLayout />}>
+              <Route index element={<HomeModule />} />
+              <Route path={PATH.MOVIE_DETAILS} element={<Details />} />
+              <Route path={PATH.BOOKING_MOVIE} element={<BookingPagse />} />
+              <Route path={PATH.REGISTER} element={<Register />} />
+              <Route path={PATH.LOG_IN} element={<LogIn />} />
+            </Route>
+            <Route path={PATH.ADMIN} element={<AdminLayout />}></Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </LocalizationProvider>
+    </UserProvider>
   );
 }
 
